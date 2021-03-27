21a4c8e30fe00c1ea180037ce2c3c9898f4e9b47
PRAVEEN YADAV <myinbox.praveen@gmail.com>
2021-03-27 23:33:28 +0530

    database connection added

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
 var http = require("http");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "remotemysql.com",
+    port:"3306",
+    user: "sssQw0eC4y",
+    password: "6om4HGi5cb",
+    database:"sssQw0eC4y",
+  });
+
+  con.connect(function(err) {
+    if (err) throw err; 
+    console.log("Connected!");
+    //create table
+    // var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    // con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Table created");
+    // });
+
+    //insert
+    // var  sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    // con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("1 record inserted");
+    // });
+
+    // con.query("SELECT * FROM customers", function (err, result, fields) {
+    // if (err) throw err;
+    // console.log(result);
+    // });
+
+  });
+
 const PORT = process.env.PORT || 5000;
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
+   
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err; 
+    response.end(result[0]["name"]);
+        console.log(result);
+        for(var i=0;i<result.length;i++){
+            console.log(result[i]["name"]);
+        }
+    });
+   
+   //response.end(result[i]["name"]);
+
+
 }).listen(PORT);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:'+PORT+'/');
