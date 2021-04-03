38d0048ca0e419b4ac702350cb8c9d5c02a79371
PRAVEEN YADAV <myinbox.praveen@gmail.com>
2021-04-03 22:36:27 +0530

    Master controller is added 04

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,9 @@ const auth=require('../controllers/auth/login');
 
 const user=require('../models/user');
 //const gst=require('../models/gst');
-const uom=require('../models/uom');
-const product=require('../models/product');
-const fk=require('../models/fk');
+//const uom=require('../models/uom');
+//const product=require('../models/product');
+//const fk=require('../models/fk');
 
 
 router.use('/isUserExist',auth.isUserExist);    
